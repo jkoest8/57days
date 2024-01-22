3ed56b40ec21b9cd04f0670edb48f3d0e41f3291
jkoest8 <146107397+jkoest8@users.noreply.github.com>
2024-01-22 11:41:24 -0600

    Update dateCalculation.js

--- dateCalculation.js
+++ dateCalculation.js
@@ -1,1 +1,13 @@
 
+function calculateDate() {
+    var selectedDate = new Date(document.getElementById("selectedDate").value);
+    if (!isNaN(selectedDate.getTime())) {
+        // Subtract 57 days
+        selectedDate.setDate(selectedDate.getDate() - 57);
+
+        var date57DaysAgo = selectedDate.toDateString(); // Formatting to a readable string
+        document.getElementById("date57DaysAgo").innerHTML = date57DaysAgo;
+    } else {
+        document.getElementById("date57DaysAgo").innerHTML = "Please select a valid date.";
+    }
+}
